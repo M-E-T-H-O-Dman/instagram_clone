867dec037052a265bb8b8f1ed0658a01194ae84a
Egalitea-leaves <charlie.redwood@gmail.com>
2014-08-19 15:22:33 +0100

    first tests added

---
+++ spec/features/post_feature_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe 'posts' do
+  context 'no posts' do
+    it 'shows a message' do
+    visit '/posts'
+    expect(page).to have_content 'No posts yet'
+    end
+  end
+
+  context 'with posts' do
+    before do 
+      Post.create(title: 'Hello world')
+    end
+    
+    it 'displays a list of all posts' do
+      visit '/posts'
+      expect(page).to have_content 'Hello world'
+    end  
+  end 
+
+  describe 'creating posts' do
+    it 'adds the posts from a from' do    
+      visit '/posts'
+      click_link 'New post'
+      fill_in 'Title', with: 'A brand new post'
+      click_button 'Create post'
+
+      expect(page).to have_content 'A brand new post'
+      expect(current_path).to eq '/posts'
+    end
+
+    it 'can attach an image to a post' do
+      visit '/posts'
+      click_link 'New post'
+      fill_in 'Title', with: 'A brand new post'
+      attach_file 'Picture', Rails.root.join('spec/images/pear_programming_250.png')
+      click_button 'Create post'
+
+      expect(page).to have_cs 'img.uploaded-pic'
+    end  
+  end 
+end  
