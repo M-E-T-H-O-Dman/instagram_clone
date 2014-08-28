21e10711c6c127f978cd6e92e7ce2659f37f3d90
Egalitea-leaves <charlie.redwood@gmail.com>
2014-08-28 13:34:32 +0100

    data attr added to maps js

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,23 +1,27 @@
 $(document).ready(function(){
-  $.get('http://0.0.0.0:3000/posts/1.json', function(postInfo){
-    var map = new GMaps({
-      div: '#map',
-      lat: -12.043333,
-      lng: -77.028333
-    });
+  var postId = $('#map').data('id');
 
-    GMaps.geocode({
-      address: postInfo.address,
-      callback: function(results, status) {
-        if (status == 'OK') {
-          var latlng = results[0].geometry.location;
-          map.setCenter(latlng.lat(), latlng.lng());
-          map.addMarker({
-            lat: latlng.lat(),
-            lng: latlng.lng()
-          });
+  if(postId) {
+    $.get('/posts/' + postId + '.json', function(postInfo){
+      var map = new GMaps({
+        div: '#map',
+        lat: -12.043333,
+        lng: -77.028333
+      });
+
+      GMaps.geocode({
+        address: postInfo.address,
+        callback: function(results, status) {
+          if (status == 'OK') {
+            var latlng = results[0].geometry.location;
+            map.setCenter(latlng.lat(), latlng.lng());
+            map.addMarker({
+              lat: latlng.lat(),
+              lng: latlng.lng()
+            });
+          }
         }
-      }
-    });
-  })  
+      });
+    }) 
+  }; 
 });
